0e4500d11782facf8fcb7be7f4ecafc6b39f1967
Ruben Perez Feijoo <rubenperezfeijoo@Rubens-MacBook-Pro.local>
2019-11-19 11:07:04 -0500

    reach router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { render } from 'react-dom'
 import SearchParams from './SearchParams'
+import {Router} from '@reach/router'
+import Details from './Details'
+import {Link} from '@reach/router'
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Link to='/'>
+          <header>Adopt Me!</header>
+        </Link>
+      <Router>
+        <SearchParams path='/' />
+        <Details path ='/details/:id' />
+
+      </Router>
     </div>
   )
 }
