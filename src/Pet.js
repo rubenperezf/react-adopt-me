20e3688a579b113214bdc73386d835e53fcd7ad6
Ruben Perez Feijoo <rubenperezfeijoo@Rubens-MacBook-Pro.local>
2019-11-19 10:27:17 -0500

    with display pictures

--- src/Pet.js
+++ src/Pet.js
@@ -1,34 +1,25 @@
+  
 import React from 'react'
 
+const Pet = props => {
+  const { name, animal, breed, media, location, id } = props
 
-const Pet = ({name, animal, breed}) => {
-    return (
-        <div>
-            <h1>Name: {name}</h1>
-            <h2>{animal}</h2>
-            <h2>{breed}</h2>
-        </div>
-    )
+  let hero = 'http://placecorgi.com/300/300'
+  if (media.length) {
+    hero = media[0].small
+  }
+
+  return (
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} — ${breed} — ${location}`}</h2>
+      </div>
+    </a>
+  )
 }
-
-
-// const Pet = props => {
-// return (
-//     <div>
-//         <h1>{props.name}</h1>
-//         <h2>{props.animal}</h2>
-//         <h2>{props.breed}</h2>
-//     </div>
-
-// )
-
-//   return React.createElement(
-//     'div',
-//     {},
-//     React.createElement('h1', {}, props.name),
-//     React.createElement('h2', {}, props.animal),
-//     React.createElement('h2', {}, props.breed)
-//   )
-//}
 
 export default Pet
